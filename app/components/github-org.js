aa685b6643808a6f6640485e474ac3b7fce55864
Artem Grachov <grachov2195@gmail.com>
2019-09-17 17:12:32 +0300

    Is org favorite computed value

--- app/components/github-org.js
+++ app/components/github-org.js
@@ -1,10 +1,14 @@
+import { inject } from '@ember/service';
 import Component from '@ember/component';
+import isInArray from '../utils/is-in-array';
 
 export default Component.extend({
   tagname: 'li',
+  favorites: inject(),
+  isFavorited: isInArray('org', 'favorites.items'),
   actions: {
     favoriteClicked() {
-      this.sendAction('on-fav-clicked', this.org)
+      this.sendAction('on-fav-clicked', this.org);
     }
   }
 });
